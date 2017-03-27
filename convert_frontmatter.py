4a288cb9f657dcf525eb4c57e061ad803989c945
Brent Stradling <brent.stradling@gmail.com>
2017-03-27 12:33:31 -0500

    updated post covert tools :)

---
+++ convert_frontmatter.py
@@ -0,0 +1,38 @@
+import os
+filedir = "_posts/"
+dirs = os.listdir(filedir)
+
+for postfile in dirs:
+	post = '_posts/' + postfile
+
+	def addFrontMatter():
+		post = '_posts/' + postfile
+		file = open(post, 'r+')
+		yt = file.read()
+		ytcode = yt.partition('www.youtube.com/embed/')[2].partition('"')[0]
+		newline = yt.replace('---', '---\nyoutube_code: '+ ytcode + '\nexcerpt:\nenclosure:\npullquote:\nenclosure_type: video/mp4\nenclosure_time:\nuse_youtube_image: true\nyoutube_alternate_image: /img/cover.jpg', 1)
+		file.seek(0)
+		file.write(newline)
+		file.close()
+
+	def cleanPost():
+		post = '_posts/' + postfile
+		file = open(post, 'r+')
+		yt = file.read()
+		frontmatter = yt.rpartition('---')[0]
+		post = yt.partition('</iframe>')[2]
+		ytpost = "---\n{% include youtube.html %}\n" + post
+		fullpost = (frontmatter + ytpost)
+		file.seek(0)
+		file.write(fullpost)
+		file.close()
+
+	def removePost():
+		if post.endswith('.html'):
+			os.remove(post)
+
+	removePost()
+	if post.endswith('.md') and post != '_posts/_defaults.md':
+		addFrontMatter()
+		cleanPost()
+		print('Post Setup Complete for ' + post)
