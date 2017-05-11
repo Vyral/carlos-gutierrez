b58151976c86f69c978e01428bf73b9be3d66907
emmadrews <drews.emma@gmail.com>
2017-05-11 15:58:09 -0500

    adding second sidebar cta and fixing visited links

--- _site/convert_frontmatter.py
+++ _site/convert_frontmatter.py
@@ -2,22 +2,25 @@ import os
 filedir = "_posts/"
 dirs = os.listdir(filedir)
 
+
 for postfile in dirs:
 	post = '_posts/' + postfile
 
 	def addFrontMatter():
 		post = '_posts/' + postfile
-		file = open(post, 'r+')
+		file = open(post, 'r+', encoding='utf-8', errors='ignore')
 		yt = file.read()
-		ytcode = yt.partition('www.youtube.com/embed/')[2].partition('"')[0]
+		ytcode = yt.partition('https://img.youtube.com/vi/')[2].partition('"')[0]
+		ytcode = ytcode[:11]
 		newline = yt.replace('---', '---\nyoutube_code: '+ ytcode + '\nexcerpt:\nenclosure:\npullquote:\nenclosure_type: video/mp4\nenclosure_time:\nuse_youtube_image: true\nyoutube_alternate_image: /img/cover.jpg', 1)
+		file = open(post, 'w')
 		file.seek(0)
 		file.write(newline)
 		file.close()
 
 	def cleanPost():
 		post = '_posts/' + postfile
-		file = open(post, 'r+')
+		file = open(post, 'r+', encoding='utf-8', errors='ignore')
 		yt = file.read()
 		frontmatter = yt.rpartition('---')[0]
 		post = yt.partition('</iframe>')[2]
